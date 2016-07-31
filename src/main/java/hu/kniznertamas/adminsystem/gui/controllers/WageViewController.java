4cc2a04ded04134f424546d563c79e05d8ed1069
Knizner Tamás <knitomi90@inf.elte.hu>
2016-07-31 16:51:15 +0200

    bérlista nézet béta

---
+++ src/main/java/hu/kniznertamas/adminsystem/gui/controllers/WageViewController.java
@@ -0,0 +1,47 @@
+package hu.kniznertamas.adminsystem.gui.controllers;
+
+import hu.kniznertamas.adminsystem.helper.WageEntityHelper;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WageViewController implements Initializable {
+
+    @FXML
+    private ComboBox yearPicker;
+
+    @FXML
+    private ComboBox monthPicker;
+
+    @FXML
+    private TableView<WageEntityHelper> wageTable;
+
+    public WageViewController() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initYearPicker();
+        initMonthPicker();
+    }
+
+    private void initYearPicker() {
+        yearPicker.getSelectionModel().selectLast();
+    }
+
+    private void initMonthPicker() {
+        monthPicker.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    private void loadTableAction() {
+
+    }
+
+
+}
---
+++ src/main/java/hu/kniznertamas/adminsystem/helper/WageEntityHelper.java
@@ -0,0 +1,10 @@
+package hu.kniznertamas.adminsystem.helper;
+
+public class WageEntityHelper {
+
+    private String userName;
+    private Integer monthlyWage;
+    private Integer prepay;
+    private Integer finalSum;
+
+}
